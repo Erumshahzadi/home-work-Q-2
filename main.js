60a6cbbf207641f8428127138f355b0373d00246
Erum shahzadi <163527258+Erumshahzadi@users.noreply.github.com>
2024-04-08 11:07:56 +0500

    vacation work by Sir Hamza (9 to 12 am)

---
+++ main.js
@@ -0,0 +1,7 @@
+// Question2:Write a program that checks if a number is even or odd.
+var num1 = 5;
+if (num1 % 2 != 0) {
+    console.log("Given Number is Odd");
+}
+else
+    console.log("Given Number is Even");
